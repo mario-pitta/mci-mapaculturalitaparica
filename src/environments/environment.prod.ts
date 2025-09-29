412838b84cbb9e58d8e0fb9ad08142b46d61f0c2
Mario Pitta <pitta_mario@hotmail.com>
2025-09-29 09:13:34 -0300

    :bug: fix: update env prod emailjs keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
-  googleMapsKey: "AIzaSyBuy4K3TmiArHSftGcbCzlVB7Lv_zWYynM",
+  googleMapsKey: 'AIzaSyBuy4K3TmiArHSftGcbCzlVB7Lv_zWYynM',
+  emailjs_pk: 'IpeSv2jf1Qy_0b7cA',
+  emailjsId: 'service_9vanxm9',
+  emailjsTemplateId: 'template_4mj7mvs',
 };
