daf03df3d17f24fde1ea63e88bacdbdb6fb0632a
Mario Pitta <pitta_mario@hotmail.com>
2025-10-10 08:28:03 -0300

    :recycle: fix:  :update contacts to instagram and mapa cultural email

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-// import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
+import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
 import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-footer',
@@ -28,52 +28,52 @@ export class FooterComponent implements OnInit {
       return;
     }
 
-    // const response: EmailJSResponseStatus = await emailjs.send(
-    //   environment.emailjsId,
-    //   environment.emailjsTemplateId,
-    //   {
-    //     title: 'Bem vindo ao Mapa Cultural de Itaparica',
-    //     subject: 'Mapa Cultural de Itaparica',
-    //     name: 'Olá, '+ this.form.value.name,
-    //     email: this.form.value.email,
-    //     sender: 'luademareproducoes@gmail.com',
-    //     sender_name: 'Lua de Maré Produções',
-    //     message: `
-    //     Venha fazer parte do Mapa Cultural de Itaparica, o maior diretorio de eventos, instituições e agentes culturais de Itaparica!
+    const response: EmailJSResponseStatus = await emailjs.send(
+      environment.emailjsId,
+      environment.emailjsTemplateId,
+      {
+        title: 'Bem vindo ao Mapa Cultural de Itaparica',
+        subject: 'Mapa Cultural de Itaparica',
+        name: 'Olá, '+ this.form.value.name,
+        email: this.form.value.email,
+        sender: 'mapaculturaldeitaparica@gmail.com',
+        sender_name: 'Lua de Maré Produções',
+        message: `
+        Venha fazer parte do Mapa Cultural de Itaparica, o maior diretorio de eventos, instituições e agentes culturais de Itaparica!
 
-    //     MCI | Instituições Culturais: https://forms.gle/7ztL1jPh8eGU3oXf7
+        MCI | Instituições Culturais: https://forms.gle/7ztL1jPh8eGU3oXf7
 
-    //     MCI | Agentes Culturais: https://forms.gle/ZvVSQ65tjjerqFDZ8
+        MCI | Agentes Culturais: https://forms.gle/ZvVSQ65tjjerqFDZ8
 
-    //     👉 O que é agente cultural?
-    //     Consideramos agentes culturais todas as pessoas que atuam na produção, preservação, transmissão ou difusão de saberes e práticas culturais: mestres e mestras de tradições, artistas, produtores, gestores, coletivos, pesquisadores e demais trabalhadores da cultura.
-
-
-    //     🏛️ O que é instituição cultural?
-    //     Instituições culturais são espaços ou organizações que promovem, preservam ou disseminam a cultura local, como escolas de música, bibliotecas, centros culturais, grupos de dança, associações comunitárias e outras iniciativas que fortalecem a vida cultural da ilha.
+        👉 O que é agente cultural?
+        Consideramos agentes culturais todas as pessoas que atuam na produção, preservação, transmissão ou difusão de saberes e práticas culturais: mestres e mestras de tradições, artistas, produtores, gestores, coletivos, pesquisadores e demais trabalhadores da cultura.
 
 
-    //     🗺️ O Mapa Cultural de Itaparica é uma iniciativa que visa identificar, valorizar e dar visibilidade à diversidade de agentes e instituições que fortalecem a cultura local. Sua participação na pesquisa é essencial para que possamos divulgar, preservar e conectar os diferentes territórios culturais da ilha, ampliando a circulação de informações e fortalecendo as redes de colaboração.
-    //     É fácil e rápido! Preencha o formulário adequado a você através dos links abaixo:
+        🏛️ O que é instituição cultural?
+        Instituições culturais são espaços ou organizações que promovem, preservam ou disseminam a cultura local, como escolas de música, bibliotecas, centros culturais, grupos de dança, associações comunitárias e outras iniciativas que fortalecem a vida cultural da ilha.
 
-    //     MCI | Instituições Culturais: https://forms.gle/7ztL1jPh8eGU3oXf7
 
-    //     MCI | Agentes Culturais: https://forms.gle/ZvVSQ65tjjerqFDZ8
-    //   `,
-    //   },
-    //   {publicKey: environment.emailjs_pk}
-    // );
+        🗺️ O Mapa Cultural de Itaparica é uma iniciativa que visa identificar, valorizar e dar visibilidade à diversidade de agentes e instituições que fortalecem a cultura local. Sua participação na pesquisa é essencial para que possamos divulgar, preservar e conectar os diferentes territórios culturais da ilha, ampliando a circulação de informações e fortalecendo as redes de colaboração.
+        É fácil e rápido! Preencha o formulário adequado a você através dos links abaixo:
 
-    // console.log(response);
-    // if (response.status === 200) {
-    //   this.sendedEmails.push(this.form.value.email);
-    //   alert(
-    //     'Mensagem enviada com sucesso! Obrigado por participar do Mapa Cultural de Itaparica! 😊.'
-    //   );
-    // } else {
-    //   alert(
-    //     'Ocorreu um erro ao enviar a mensagem. Por favor, tente novamente.'
-    //   );
-    // }
+        MCI | Instituições Culturais: https://forms.gle/7ztL1jPh8eGU3oXf7
+
+        MCI | Agentes Culturais: https://forms.gle/ZvVSQ65tjjerqFDZ8
+      `,
+      },
+      {publicKey: environment.emailjs_pk}
+    );
+
+    console.log(response);
+    if (response.status === 200) {
+      this.sendedEmails.push(this.form.value.email);
+      alert(
+        'Mensagem enviada com sucesso! Obrigado por participar do Mapa Cultural de Itaparica! 😊.'
+      );
+    } else {
+      alert(
+        'Ocorreu um erro ao enviar a mensagem. Por favor, tente novamente.'
+      );
+    }
   }
 }
